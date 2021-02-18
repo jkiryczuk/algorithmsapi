cd5926f8305b4598b890c05f7863b9afecba030d
Jerzy Kiryczuk <jurkirk@gmail.com>
2021-02-18 18:14:56 +0100

    initial project from start.spring.io

---
+++ src/main/java/pl/jerzykiryczuk/algorithmapi/AlgorithmapiApplication.java
@@ -0,0 +1,13 @@
+package pl.jerzykiryczuk.algorithmapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/jerzykiryczuk/algorithmapi/AlgorithmapiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jerzykiryczuk.algorithmapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
